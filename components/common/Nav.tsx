0a849770c622dfbfd4d24a9c0f1015514965b409
Chinh Do <do.inby@gmail.com>
2022-07-06 22:35:51 +1000

    Add home link to logo

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 /* eslint-disable @next/next/no-img-element */
 export default function Nav() {
   return (
     <nav className='justify-between items-center'>
-      <img src='/assets/shared/desktop/logo.svg' alt='Logo' />
+      <Link href='/'>
+        <img src='/assets/shared/desktop/logo.svg' alt='Logo' />
+      </Link>
       <button className='p-0'>
         <img
           src='/assets/shared/mobile/icon-hamburger.svg'
