c2bc09a42d31dbdcfe8878534d01d518c45d6cdc
Chinh Do <do.inby@gmail.com>
2022-07-07 09:32:22 +1000

    Change to white background. Add TS specification for LocationCard.tsx

--- components/about/LocationCard.tsx
+++ components/about/LocationCard.tsx
@@ -1,9 +1,7 @@
-import {ReactNode} from 'react';
-
 type LocationCardProps = {
   imgUrl: string;
   name: string;
-  address: ReactNode;
+  address: JSX.Element;
 };
 
 export default function LocationCard(props: LocationCardProps) {
@@ -13,7 +11,7 @@ export default function LocationCard(props: LocationCardProps) {
     <article className='card text-center place-items-center py-6'>
       <img src={imgUrl} alt={name} className='w-12 mb-8' />
       <h3>{name}</h3>
-      <p>{address}</p>
+      {address}
     </article>
   );
 }
