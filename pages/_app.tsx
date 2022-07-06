c823dc2e318493a95b69949bdb128191aaeb6f42
Chinh Do <do.inby@gmail.com>
2022-07-06 22:35:34 +1000

    Move <Head> tag to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import './globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import type {AppProps} from 'next/app';
+import './globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Coffeeroaster | DIY coffee subscription plan</title>
+        <meta
+          name='description'
+          content='Making coffee bean top-up easy since 1983'
+        />
+        <link rel='icon' href='/favicon-32x32.png' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
