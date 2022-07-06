0e34f6a94e44373c511642f58da4bd15a359bbf9
Chinh Do <do.inby@gmail.com>
2022-07-06 10:19:11 +1000

    Add hero section

---
+++ components/home/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <section
+      id='hero-section'
+      className='bg-hero-img bg-center bg-cover bg-no-repeat place-items-center rounded-lg'
+    >
+      <h2 className='text-white'>Great coffee made simple.</h2>
+      <p className='text-center'>
+        Start your mornings with the world’s best coffees. Try our expertly
+        curated artisan coffees from our best roasters delivered directly to
+        your door, at your schedule.
+      </p>
+      <button className='btn-primary'>Create your plan</button>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Barlow', 'sans-serif'],
+      serif: ['Fraunces', 'serif'],
+    },
+    colors: {
+      'dark-cyan': '#0E8784',
+      'light-cyan': '#66D2CF',
+      'dark-blue': '#333D4B',
+      'pale-orange': '#FDD6BA',
+      'light-cream': '#FEFCF7',
+      'dark-cream': '#F4F1EB',
+      'light-gray': '#E2DEDB',
+      'neutral-gray': '#83888F',
+      white: '#FFF',
+      black: '#000',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-img': "url('/assets/home/mobile/image-hero-coffeepress.jpg')",
+      },
+    },
   },
   plugins: [],
 };
